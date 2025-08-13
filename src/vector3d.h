d8ff08cbb40c1373c64defb45f8ff832d9864999
Andrew Vataev <avataev@yandex.ru>
2025-08-13 17:01:49 +0300

    добавлен деструктор

--- src/vector3d.h
+++ src/vector3d.h
@@ -9,20 +9,23 @@ class Vector3D
 private:
     // Объявим закрытые переменные класса
     std::vector<double> coords_;
-    int n_; //номер элемента 
+    int n_; // номер элемента
 
 public:
     // Конструктор без параметров
     Vector3D() {}
 
-    //Конструктор с параметрами
+    // Конструктор с параметрами
     Vector3D(int n) : n_(n), coords_(std::vector<double>(n)) {};
 
-    //перегрузка оператора доступа к элементам матрицы
-    double& operator()(int row); // возврат значения по ссылке (знак &)
+    // Деструктор
+    ~Vector3D() { std::cout<<"Item destroyed\n"; }
+
+    // перегрузка оператора доступа к элементам матрицы
+    double &operator()(int row); // возврат значения по ссылке (знак &)
 
     void print();
 
     // перегрузка операции ввода элементов вектора
-    //friend std::istream &operator>>(std::istream &in, Vector3D &v);
+    // friend std::istream &operator>>(std::istream &in, Vector3D &v);
 };
