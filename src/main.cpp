3a0532d5c0c654c6d2d7a9b0e70f87b2966a9bd1
Andrew Vataev <avataev@yandex.ru>
2025-08-13 14:39:14 +0300

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "vector3d.h"
+
+void main()
+{
+    std::cout<<"hello"<<std::endl;
+}
---
+++ src/vector3d.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Создадим класс для хранения координат вектора (coords)
+class Vector3D
+{
+private:
+    // Объявим закрытые переменные класса
+    std::vector<double> coords_;
+
+public:
+    // Конструктор без параметров
+    Vector3D() {}
+    
+};
